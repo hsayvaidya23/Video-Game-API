d9d98be8a407fa0b7d4000be38e35b5ea1c83965
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-26 16:35:53 +0530

    Displaying the games

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-
   const [gameTitle, setGameTitle] = useState("");
+  const [searchedGames, setSearchedGames] = useState([]);
 
   const searchGame = () => {
-    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSearchedGames(data);
+      });
   };
 
   return (
     <div className="App">
       <div className="searchSection">
         <h1>Search For A Game</h1>
-        <input type="text " placeholder='Minecraft...' onChange={(event) => { setGameTitle(event.target.value)}} />
+        <input
+          type="text "
+          placeholder="Minecraft..."
+          onChange={(event) => {
+            setGameTitle(event.target.value);
+          }}
+        />
         <button onClick={searchGame}>Search Game Title</button>
+        <div className="games">
+          {searchedGames.map((game, key) => {
+            return <div className="game" key={key}>{game.external} </div>
+          })}
+        </div>
       </div>
       <div className="dealsSection">
         <h1>Latest Deals</h1>
